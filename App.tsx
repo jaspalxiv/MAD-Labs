4bfaedadb538a0f14097a57d6245999c0a0f96e7
jaspalxiv@gmail.com <jaspalxiv@gmail.com>
2025-01-23 16:38:10 -0700

    MAD-Lab1

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Yoy may Press</Text>
+            <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Realy", "Nuke, you should not have press")}>
+                <Text>Press me</Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: 'red',
+        padding: 10,
+        borderRadius: 10,
+        width: 100,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center'
+
+    }
 });
